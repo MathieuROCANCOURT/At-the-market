eccd81a5acaf97c3ed18223efd1a35dd28a130ee
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-18 11:42:04 +0200

    feat: Add constructor and the methods from Consumable interface.

--- At_the_market/src/market/FruitVetetable.java
+++ At_the_market/src/market/FruitVetetable.java
@@ -3,9 +3,46 @@
  */
 package market;
 
+import java.time.LocalDate;
+
 /**
  * 
  */
 public class FruitVetetable extends Product implements Consumable {
+	/**
+	 * @param name
+	 * @param unitPrice
+	 * @param unit
+	 * @param stockQuantity
+	 * @param pickingDate
+	 * @param shelfLifeDays
+	 */
+	public FruitVetetable(String name, double unitPrice, String unit, double stockQuantity, LocalDate pickingDate,
+			int shelfLifeDays) {
+		super(name, unitPrice, unit, stockQuantity, pickingDate, shelfLifeDays);
+	}
 
+	@Override
+	public boolean isRipe() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isExpired(LocalDate dateVerification) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long daysRemainingBeforeExpiration(LocalDate dateVerification) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void calculateExpirationDate() {
+		// TODO Auto-generated method stub
+		
+	}
 }
