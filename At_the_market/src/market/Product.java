5b52f6f2e8ff8a7175ffad2ca0bcddb197ea4e80
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-18 11:08:21 +0200

    feat: Add a constructor.

--- At_the_market/src/market/Product.java
+++ At_the_market/src/market/Product.java
@@ -3,9 +3,40 @@
  */
 package market;
 
+import java.time.LocalDate;
+
 /**
  * 
  */
 public abstract class Product {
+	private String name;
+	private double unitPrice;
+	private String unit;
+	private double stockQuantity;
+	private LocalDate pickingDate;
+	private int shelfLifeDays;
 
+	/**
+	 * @param name
+	 * @param unitPrice
+	 * @param unit
+	 * @param stockQuantity
+	 * @param pickingDate
+	 * @param shelfLifeDays
+	 */
+	public Product(String name, double unitPrice, String unit, double stockQuantity, LocalDate pickingDate,
+			int shelfLifeDays) {
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.unit = unit;
+		this.stockQuantity = stockQuantity;
+		this.pickingDate = pickingDate;
+		this.shelfLifeDays = shelfLifeDays;
+	}
+	
+	public abstract void calculateExpirationDate();
+	
+	public void updateProduct() {
+		
+	}
 }
