caf59de62206f9f37647626e1d46bc8c656d5004
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-18 11:18:46 +0200

    chore: Add isRipe, isExpired and daysRemainingBeforeExpiratio methods.

--- At_the_market/src/market/Consumable.java
+++ At_the_market/src/market/Consumable.java
@@ -3,9 +3,29 @@
  */
 package market;
 
+import java.time.LocalDate;
+
 /**
  * 
  */
 public interface Consumable {
+	/**
+	 * @return true if is ripe, else false.
+	 */
+	public boolean isRipe();
+
+	/**
+	 * 
+	 * @param dateVerification
+	 * @return
+	 */
+	public boolean isExpired(LocalDate dateVerification);
+	
+	/**
+	 * 
+	 * @param dateVerification
+	 * @return
+	 */
+	public long daysRemainingBeforeExpiration(LocalDate dateVerification);
 
 }
