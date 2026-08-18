bac545fa41fe6d4771dc0f0cba65a7b91ed36cc8
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-18 14:06:37 +0200

    docs: Complete docstring on the constructor.

--- At_the_market/src/market/FruitVegetable.java
+++ At_the_market/src/market/FruitVegetable.java
@@ -4,19 +4,18 @@
 package market;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 /**
  * 
  */
 public class FruitVegetable extends Product implements Consumable {
 	/**
-	 * @param name
-	 * @param unitPrice
-	 * @param unit
-	 * @param stockQuantity
-	 * @param pickingDate
-	 * @param shelfLifeDays
+	 * @param name          Fruit or vegetable name
+	 * @param unitPrice     Price to piece or kg
+	 * @param unit          "piece" or "kg"
+	 * @param stockQuantity Quantity available in the shop
+	 * @param pickingDate   Harvest date
+	 * @param shelfLifeDays Number of days to keep
 	 */
 	public FruitVegetable(String name, double unitPrice, String unit, double stockQuantity, LocalDate pickingDate,
 			int shelfLifeDays) {
