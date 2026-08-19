1a455ba9a7a801b1e59d4ea32dc76167f4cbb414
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-19 14:31:52 +0200

    chore: Add fields in Customer class.

--- At_the_market/src/market/Customer.java
+++ At_the_market/src/market/Customer.java
@@ -3,9 +3,13 @@
  */
 package market;
 
+import java.util.ArrayList;
+
 /**
  * 
  */
 public class Customer {
-
+	private String firstName;
+	private String lastName;
+	private ArrayList<Product> productList = new ArrayList<>();
 }
