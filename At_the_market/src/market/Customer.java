c21f286eb599fce466c8a101ba362abfeb1acb92
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-19 14:33:48 +0200

    feat: Add Customer constructor.

--- At_the_market/src/market/Customer.java
+++ At_the_market/src/market/Customer.java
@@ -11,5 +11,10 @@ import java.util.ArrayList;
 public class Customer {
 	private String firstName;
 	private String lastName;
-	private ArrayList<Product> productList = new ArrayList<>();
+	private ArrayList<Product> basket = new ArrayList<>();
+	
+	public Customer(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 }
